ea374599d6a66733989d88c53d5c55a748d43335
Sergey Matorykin <smatorykin@outlook.com>
2024-08-17 20:28:36 +0500

    combining methods executeCommandWithoutLog and executeCommand

--- src/terminal/terminalwrapper.ts
+++ src/terminal/terminalwrapper.ts
@@ -13,34 +13,7 @@ export class TerminalWrapper {
 		this.executeResultFilePath = `${this.outputPathLog}\\${this.executeResultFileName}`;
 	}
 
-	public async executeCommandWithoutLog(command: string): Promise<vscode.TerminalExitStatus> {
-		let terminal = vscode.window.terminals.find(i => i.name === this.terminalName);
-		if (!terminal) {
-			terminal = vscode.window.createTerminal({
-				name: this.terminalName,
-				location: vscode.TerminalLocation.Panel,
-			});
-		}
-		terminal.show(true);
-		terminal.sendText(command, false);
-		terminal.sendText("; exit");
-		return new Promise((resolve, reject) => {
-			const disposeToken = vscode.window.onDidCloseTerminal(
-				async (closedTerminal) => {
-					if (closedTerminal === terminal) {
-						disposeToken.dispose();
-						if (terminal.exitStatus !== undefined) {
-							resolve(terminal.exitStatus);
-						} else {
-							reject("Terminal exited with undefined status");
-						}
-					}
-				}
-			);
-		});
-	}
-
-	public async executeCommand(command: string): Promise<boolean> {
+	public async executeCommand(command: string, isLogging: boolean): Promise<boolean> {
 		let terminal = vscode.window.terminals.find(i => i.name === this.terminalName);
 		if (!terminal) {
 			terminal = vscode.window.createTerminal({
@@ -50,22 +23,30 @@ export class TerminalWrapper {
 		}
 		
 		terminal.show(true);
-		terminal.sendText(`$share = ${this.setEncodingUtf8} ${command} | Tee-Object -file ${this.executeLogFilePath}; `, false);
-		terminal.sendText(`if($?){'1' > ${this.executeResultFilePath}}else{'0' > ${this.executeResultFilePath}}; `, false);
-		terminal.sendText("exit");
+		if(isLogging){
+			terminal.sendText(`$share = ${this.setEncodingUtf8} ${command} | Tee-Object -file ${this.executeLogFilePath}; `, false);
+			terminal.sendText(`if($?){'1' > ${this.executeResultFilePath}}else{'0' > ${this.executeResultFilePath}} `, false);
+		}else{
+			terminal.sendText(command, false);
+		}
+		terminal.sendText(" ;exit");
 		return new Promise((resolve, reject) => {
 			const disposeToken = vscode.window.onDidCloseTerminal(
 				async (closedTerminal) => {
 					if (closedTerminal === terminal) {
 						disposeToken.dispose();
-						var pathFile = vscode.Uri.file(this.executeResultFilePath);
-						const readData = await vscode.workspace.fs.readFile(pathFile);
-						const readStatus = Buffer.from(readData).toString('utf8');
 						if (terminal.exitStatus !== undefined) {
-							if (readStatus.includes('1')) {
+							if(isLogging){
+								var pathFile = vscode.Uri.file(this.executeResultFilePath);
+								const readData = await vscode.workspace.fs.readFile(pathFile);
+								const readStatus = Buffer.from(readData).toString('utf8');
+								if (readStatus.includes('1')) {
+									resolve(true);
+								} else {
+									resolve(false);
+								}
+							}else{
 								resolve(true);
-							} else {
-								resolve(false);
 							}
 						} else {
 							reject("Terminal exited with undefined status");
